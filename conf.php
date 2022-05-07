00eb9f0909b8567ffa3e417e1e1357e8bd641438
Ayan Neupane <aneupan1@jhu.edu>
2022-05-06 22:25:53 -0400

    added conf

---
+++ conf.php
@@ -0,0 +1,8 @@
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "dbase.cs.jhu.edu"; // database host
+	$dbuser = "22sp_aneupan1";    // username on db host: replace 22sp_jhopkin1 with yours
+	$dbpass = "6Di28aq0Ip";            // password on db host: replace ***** with yours
+	$dbname = "22sp_aneupan1_db"; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
