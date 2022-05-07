e29852564464bd871f163ad6719349910814ecd8
Ayan Neupane <aneupan1@jhu.edu>
2022-05-07 00:59:28 -0400

    basic framework for each procedure

---
+++ Procedure1.php
@@ -0,0 +1,43 @@
+#RON PRESCOTT: rpresco3, AYAN NEUPANE: aneupan1
+
+<head><title> Average gdp, house price, and population of metro areas who has had a team in every sport in the last 20 years
+</title></head>
+<body>
+<?php
+    include 'open.php';
+    $aQuery = "CALL Procedure1()";
+    
+    if ($result = mysqli_query($conn, $aQuery)) {
+       echo "<table border=\"2px solid black\">";
+
+          // output a row of table headers
+	      echo "<tr>";
+	      // collect an array holding all attribute names in $result
+	      $flist = $result->fetch_fields();
+          // output the name of each attribute in flist
+	      foreach($flist as $fname){
+	         echo "<th>".$fname->name."</th>";
+	      }
+	      echo "</tr>";
+
+          // output a row of table for each row in result, using flist names
+          // to obtain the appropriate attribute value for each column
+	      foreach($result as $row){
+
+              // reset the attribute names array
+    	      $flist = $result->fetch_fields(); 
+	          echo "<tr>";
+	          foreach($flist as $fname){
+                      echo "<td>".$row[$fname->name]."</td>";
+              }
+  	          echo "</tr>";
+	      }
+	      echo "</table>";
+    }
+    else {
+         echo "<h2> ERROR: QUERY FAILED </h2>";
+    }
+    //close the connection opened by open.php since we no longer need access to dbase
+    $conn->close();
+?>
+</body>
