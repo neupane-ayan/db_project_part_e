fad73d79c98de7025f62bf2840f1d08670e9ebdd
Ayan Neupane <aneupan1@jhu.edu>
2022-05-10 13:29:21 -0400

    added Canvas for Procedure1

--- Procedure1.php
+++ Procedure1.php
@@ -6,34 +6,19 @@
 <?php
     include 'open.php';
     $aQuery = "CALL Procedure1()";
-    
+
+    $dataPoints = array();
+    $dataPoints2 = array();
+    $dataPoints3 = array();
     if ($result = mysqli_query($conn, $aQuery)) {
-       echo "<table border=\"2px solid black\">";
-
-          // output a row of table headers
-	      echo "<tr>";
-	      // collect an array holding all attribute names in $result
-	      $flist = $result->fetch_fields();
-          // output the name of each attribute in flist
-	      foreach($flist as $fname){
-	         echo "<th>".$fname->name."</th>";
-	      }
-	      echo "</tr>";
-
-          // output a row of table for each row in result, using flist names
-          // to obtain the appropriate attribute value for each column
-	      foreach($result as $row){
-
-              // reset the attribute names array
-    	      $flist = $result->fetch_fields(); 
-	          echo "<tr>";
-	          foreach($flist as $fname){
-                      echo "<td>".$row[$fname->name]."</td>";
-              }
-  	          echo "</tr>";
-	      }
-	      echo "</table>";
+       	foreach($result as $row){
+	   $lbl = $row["metroAreaName"];
+           array_push($dataPoints, array("label"=> $lbl, "y"=> $row["avg(gdp)"]));
+	   array_push($dataPoints2, array("label"=> $lbl, "y"=> $row["avg(averageHousePrice)"]));
+	   array_push($dataPoints3, array("label"=> $lbl, "y"=> $row["avg(population)"]));
+	}      
     }
+    
     else {
          echo "<h2> ERROR: QUERY FAILED </h2>";
     }
@@ -42,3 +27,68 @@
 ?>
 </body>
 
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {  
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title: {
+		text: "Average gdp of metro areas who has had a team in every sport in the last 20 years"
+	},
+	axisY: {
+	        includeZero: true,
+		title: "GDP in Millions of $USD"
+	},
+	data: [{
+		type: "column",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+var chart2 = new CanvasJS.Chart("chartContainer2", {
+        animationEnabled: true,
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        title: {
+                text: "Average house price of metro areas who has had a team in every sport in the last 20 years"
+        },
+        axisY: {
+	        includeZero: true,
+                title: "avg house price in thousands of $USD"
+        },
+        data: [{
+                type: "column",
+                dataPoints: <?php echo json_encode($dataPoints2, JSON_NUMERIC_CHECK); ?>
+        }]
+});
+chart2.render();
+var chart3 = new CanvasJS.Chart("chartContainer3", {
+        animationEnabled: true,
+        theme: "light2", // "light1", "light2", "dark1", "dark2"
+        title: {
+                text: "Average population of metro areas who has had a team in every sport in the last 20 years"
+        },
+        axisY: {
+                includeZero: true,
+                title: "avg house population in thousands of people"
+        },
+        data: [{
+                type: "column",
+                dataPoints: <?php echo json_encode($dataPoints3, JSON_NUMERIC_CHECK); ?>
+        }]
+});
+chart3.render();
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<br>
+<div id="chartContainer2" style="height: 300px; width: 100%;"></div>
+<br>
+<div id="chartContainer3" style="height: 300px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+</body>
+</html>
