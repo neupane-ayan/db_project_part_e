32f6b57d2ad53543efd904ec9b194b7b6c8c16bf
Ayan Neupane <aneupan1@jhu.edu>
2022-05-09 23:42:47 -0400

    turned P15 into CanvasJS graph

--- Procedure1.php
+++ Procedure1.php
@@ -41,3 +41,4 @@
     $conn->close();
 ?>
 </body>
+
--- Procedure15.php
+++ Procedure15.php
@@ -1,37 +1,13 @@
 #RON PRESCOTT: rpresco3, AYAN NEUPANE: aneupan1
 
-<h2>Compare average metro-area GDP of NHL champions, NBA champions, NFL champions and MLB champions</h2>
-<body>
 <?php
     include 'open.php';
     $aQuery = "CALL Procedure15()";
-    
+    $dataPoints = array();
     if ($result = mysqli_query($conn, $aQuery)) {
-       echo "<table border=\"2px solid black\">";
-
-          // output a row of table headers
-	      echo "<tr>";
-	      // collect an array holding all attribute names in $result
-	      $flist = $result->fetch_fields();
-          // output the name of each attribute in flist
-	      foreach($flist as $fname){
-	         echo "<th>".$fname->name."</th>";
-	      }
-	      echo "</tr>";
-
-          // output a row of table for each row in result, using flist names
-          // to obtain the appropriate attribute value for each column
-	      foreach($result as $row){
-
-              // reset the attribute names array
-    	      $flist = $result->fetch_fields(); 
-	          echo "<tr>";
-	          foreach($flist as $fname){
-                      echo "<td>".$row[$fname->name]."</td>";
-              }
-  	          echo "</tr>";
-	      }
-	      echo "</table>";
+       	foreach($result as $row){
+           array_push($dataPoints, array("label"=> $row["sport"], "y"=> $row["avg(gdp)"]));
+    	}      
     }
     else {
          echo "<h2> ERROR: QUERY FAILED </h2>";
@@ -39,4 +15,34 @@
     //close the connection opened by open.php since we no longer need access to dbase
     $conn->close();
 ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+<script>
+window.onload = function () {
+ 
+var chart = new CanvasJS.Chart("chartContainer", {
+	animationEnabled: true,
+	theme: "light2", // "light1", "light2", "dark1", "dark2"
+	title: {
+		text: "Compare average metro-area GDP of NHL champions, NBA champions, NFL champions and MLB champions"
+	},
+	axisY: {
+		title: "GDP in Millions of $USD"
+	},
+	data: [{
+		type: "column",
+		dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart.render();
+ 
+}
+</script>
+</head>
+<body>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
+</html>   
